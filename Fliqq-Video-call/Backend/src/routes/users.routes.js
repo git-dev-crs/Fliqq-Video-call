dd57b84cb1c370032fa1dd686b434a57630f9c10
Mohit Jatav <mohitjatav326@gmail.com>
2025-12-25 13:42:26 +0530

    Managing the user.routs.js behaviour in the routes of the Backend server

--- Fliqq-Video-call/Backend/src/routes/users.routes.js
+++ Fliqq-Video-call/Backend/src/routes/users.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { addToHistory, getUserHistory, login, register, googleAuth, getUserDetails } from "../controllers/user.controller.js";
+import { addToHistory, getUserHistory, login, register, googleAuth, getUserDetails, updateUserDetails } from "../controllers/user.controller.js";
 
 const router = Router();
 router.route("/login").post(login);
@@ -8,5 +8,6 @@ router.route("/add_to_activity").post(addToHistory)
 router.route("/get_all_activity").post(getUserHistory)
 router.route("/oauth").post(googleAuth)
 router.route("/get_user_details").get(getUserDetails)
+router.route("/update_user_details").post(updateUserDetails)
 
 export default router;
