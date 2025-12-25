4b22a96d894e86fc8f2535967a0d8b8ce15f6712
Mohit Jatav <mohitjatav326@gmail.com>
2025-12-25 13:36:17 +0530

    fixing the logic in the app.js of the Backend server

--- Fliqq-Video-call/Backend/src/app.js
+++ Fliqq-Video-call/Backend/src/app.js
@@ -19,8 +19,8 @@ const io = connectToSocket(server);
 
 app.set("port", (process.env.PORT || 8000))
 app.use(cors());
-app.use(express.json({ limit: "40kb" }))
-app.use(express.urlencoded({ limit: "40kb", extended: true }));
+app.use(express.json({ limit: "10mb" }))
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use("/api/v1/users", userRoutes);
 
 
