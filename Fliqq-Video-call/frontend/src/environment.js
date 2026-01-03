911fecc5b3639e744b33e62fcb78b2ec79ede6ae
Mohit Jatav <mohitjatav326@gmail.com>
2026-01-04 00:49:15 +0530

    connect the Frontend API configuration and Backend CORS settings

--- Fliqq-Video-call/frontend/src/environment.js
+++ Fliqq-Video-call/frontend/src/environment.js
@@ -1,7 +1,7 @@
 // export const BASE_URL = "http://localhost:5000"; 
 // export const SOCKET_URL = "http://localhost:5000"; 
 let IS_PROD = false;
-const url = "";
+const url = "http://localhost:8000";
 
 
 export default url;
